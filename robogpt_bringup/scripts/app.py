3547b4d42e521e70bdbaa8a6531253c7b32d260a
Yuvimehta <yuvraj.m@orangewood.co>
2024-11-29 21:38:51 +0530

    added url

--- robogpt_bringup/scripts/app.py
+++ robogpt_bringup/scripts/app.py
@@ -7,8 +7,8 @@ import subprocess
 import rospkg,rospy
 from PySide6.QtWidgets import QApplication,QLineEdit, QWidget, QVBoxLayout, QLabel, QGraphicsOpacityEffect, QComboBox, QPushButton, QHBoxLayout, QGridLayout
 from PySide6.QtWidgets import QSpacerItem, QSizePolicy
-from PySide6.QtGui import QPixmap, QFont
-from PySide6.QtCore import Qt, QTimer, QPropertyAnimation
+from PySide6.QtGui import QPixmap, QFont,QDesktopServices
+from PySide6.QtCore import Qt, QTimer, QPropertyAnimation, QUrl
 
 rospack = rospkg.RosPack()
 vision_path = rospack.get_path('robogpt_vision')
@@ -318,9 +318,9 @@ class MainApp(QWidget):
             print(f"Here is the List of Arguments: {args}")
 
             bringup_process = launch_with_delay('robogpt_bringup bringup.launch',args=args, delay=5)
-            url_process = urllib.urlopen('http://example.com')
+            url = QUrl("https://robogpt.orangewood.co/")
+            QDesktopServices.openUrl(url)
             bringup_process.wait()  
-            url_process.wait()
 
         except KeyboardInterrupt:
                 # Terminate all processes if the script is interrupted
